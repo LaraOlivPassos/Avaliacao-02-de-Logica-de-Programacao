93144b0c983eb7d22b4ee56d9899babfa4618612
LaraOlivPassos <laraolivpassos@gmail.com>
2025-10-21 08:46:25 -0300

    Questão 7 - Código

---
+++ Questão 7/Questão 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicita os números inicial e final ao usuário
+            Console.Write("Digite o número inicial:");
+            int inicio = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Digite o número final:");
+            int fim = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine();
+
+            for (int i = inicio; i <= fim; i++)
+            {
+                Console.WriteLine($"Tabuada do {i}:");
+
+                for (int valorMultiplicado = 1; valorMultiplicado <= 10; valorMultiplicado++)
+                {
+                    Console.WriteLine($"{i} x {valorMultiplicado} = {i * valorMultiplicado}");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Fim da tabuada!");
+        }
+    }
